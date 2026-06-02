41febf94f2159e1921b6845c6a50ce05bdbe6c8c
jihoo32 <kellyjihoo@naver.com>
2026-06-02 22:41:50 +0900

    [test] Login API 연결 성공

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -9,9 +9,12 @@ router = APIRouter()
     response_model=LoginResponse)
 
 def login(request: LoginRequest):
-    if not request.user_id or not request.password:
+    if (
+        request.user_id != "admin"
+        or request.password != "1234"
+    ):
         raise HTTPException(
-            status_code=400,
+            status_code=401,
             detail="아이디 또는 비밀번호가 잘못되었습니다."
         )
     return {
@@ -19,7 +22,7 @@ def login(request: LoginRequest):
         "access_token": "dummy-access-token",
         "token_type": "bearer",
         "user":{
-            "id": request.user_id,
+            "id": "admin",
             "name": "김준법",
             "team": "준법자문가 연구소"
         },
